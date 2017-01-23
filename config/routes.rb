ae84907ed8fc998e90101e2b4985af28a4883878
zhanyouxin <zhanyouxin@gmail.com>
2017-01-23 14:27:08 +0800

    admin panel routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,28 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :welcome do
-
-  end
-
-  root 'welcome#index'
-
-  resources :jobs do
-    resources :resumes
-  end
-
   namespace :admin do
     resources :jobs do
       member do
         post :publish
         post :hide
+        get :search
       end
-
       resources :resumes
     end
   end
 
+  resources :welcome do
+  end
 
+  resources :jobs do
+      member do
+          get :search
+   end
+
+      resources :resumes
+    end
+
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
